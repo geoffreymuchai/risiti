ee074494668100fab7158c400d7217386c610d4d
geoffreymuchai <geoffreymuchai@gmail.com>
2013-09-27 06:37:32 +0300

    Add unicode descriptions to receipt models

--- receipts/models.py
+++ receipts/models.py
@@ -9,19 +9,31 @@ class Receipt(models.Model):
 	price = models.DecimalField(max_digits=9, decimal_places=2)
 	recepient_image = models.ImageField()
 
+	def __unicode__(self):
+		return self.description
+
 class Merchant(models.Model):
 	name = models.CharField(max_length=255)
 	description = models.TextField()
 	total_amount_debited = models.DecimalField(max_digits=9, decimal_places=2)
 
+	def __unicode__(self):
+		return self.name
+
 class Category(models.Model):
 	name = models.CharField(max_length=255)
 	value = models.CharField(max_length=255)
+
+	def __unicode__(self):
+		return self.name
 
 class Account(models.Model):
 	name = models.CharField(max_length=255)
 	description = models.TextField()
 	account_type = models.CharFields(max_length=255)
 	amount = models.DecimalField(max_digits=9, decimal_places=2)
+
+	def __unicode__(self):
+		return self.name
 
 
