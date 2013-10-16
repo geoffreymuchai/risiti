a7a8bb1351fe0f51339901745785fe33c57cdd02
geoffreymuchai <geoffreymuchai@gmail.com>
2013-10-16 22:21:00 +0300

    Add ReceiptIndexView and ReceiptDetailView

--- receipts/views.py
+++ receipts/views.py
@@ -1,12 +1,19 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.core.urlresolvers import reverse
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 
-from django.utils import timezone
-
-from polls.models import Question, Choice
+from receipts.models import Receipt
 
 
 def index(request):
 	render(request, 'risiti/index.html')
+
+
+class ReceiptIndexView(generic.ListView):
+	queryset = Receipt.objects.order_by('-date_created')
+	context_object_name = "receipt_list"
+	template_name = "receipts/index.html"
+
+class ReceiptDetailView(generic.DetailView):
+	model = Receipt
+	template_name = "receipts/detail.html"
